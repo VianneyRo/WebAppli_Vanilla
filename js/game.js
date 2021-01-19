7d0d112da5effac9660b06afd3eeef170019b7fb
Viro <rousselotvianney@gmail.com>
2021-01-19 15:47:57 +0100

    Resolution prb affichage cercles

--- js/game.js
+++ js/game.js
@@ -6,22 +6,39 @@ canvas.height = document.documentElement.clientHeight || document.body.clientHei
 
 document.addEventListener("DOMCContentLoaded", startUp);
 
-function createSquares(x, y, w, h, color, dx, dy, draw){
-    let obj = {
-        x : 50,
-        y : 10,
-        w : 150,
-        h : 100,
-        color : 'red',
-        dx : 2,
-        dy : 2,
-        draw : rect_draw
+function rect_create(x, y, w, h, color, dx, dy) {
+    let objRect = {
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        color: color,
+        dx: dx,
+        dy: dy,
+        draw: rect_draw
     }
-    return obj;
+    return objRect;
 }
 
-let rect = createSquares(10, 20, 30, 50, 'red', 3, 3);
-let rect2 = createSquares(100, 20, 30, 50, 'yellow', 2, 3);
+function cercle_create(x, y, rayon, color, angleStart, angleEnd, sensAntiHoraire) {
+    console.log("Create new circless");
+    let objCercle = {
+        x: x,
+        y: y,
+        rayon: rayon,
+        color: color,
+        angleStart: angleStart,
+        angleEnd: angleEnd,
+        sensAntiHoraire: sensAntiHoraire,
+        offset : Math.random * Math.PI * 2
+    }
+    return objCercle;
+}
+
+let rect = rect_create(10, 20, 30, 50, 'red', 3, 5);
+let rect2 = rect_create(100, 20, 30, 50, 'green', 3, 5);
+
+const colors = ['blue', 'red', 'yellow'];
 
 let x = 0;
 let y = 0;
@@ -37,77 +54,94 @@ var listSquarres = [];
 var img = new Image();
 img.src = 'pictures/SpongeBob.png'
 
-function startUp(){
+function startUp() {
     el.addEventListener("touchstart", handleStart, false);
     el.addEventListener("touchend", handleEnd, false);
     el.addEventListener("touchcancel", handleCancel, false);
     el.addEventListener("touchmove", handleMove, false);
 }
 
-function gameLoop(){
+const gameObjects = [];
+let frame = 0;
+
+function generateCircle(){
+    for(let i = 0; i < 50; i++){
+        // Place des cercles à des endroits aléatoires
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        // Rayon aléatoire
+        const r = Math.random() * 100 + 50;
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        // Ajoute un nouveau cerlcle au tableau gameobjects
+        gameObjects.push(cercle_create(x, y, r, color, 3, 8, Math.random() >= 0.5));
+    }
+}
+
+function gameLoop() {
     ctx.fillStyle = 'green';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.w, this.h);    
-
+    for(const obj of gameObjects){
+        circle_print(obj);
+    }
+    frame++;
     // ctx.fillStyle = 'red';
     //ctx.fillRect(x, y, CUBE_SIZE_X, CUBE_SIZE_Y);
 
-    //ctx.globalCompositeOperation = "destination-in";
-
-    ctx.drawImage(img, x, y);
+    //ctx.drawImage(img, x, y);
 
     //var imgData = ctx.getImageData(x, y, canvas.width, canvas.height);
 
-    if(x + img.width + moveX > canvas.width || x + moveX <= 0){
+    /*if (x + img.width + moveX > canvas.width || x + moveX <= 0) {
         moveX *= -1;
+    }*/
 
-        /*for(var i = 0; i < imgData.data.length; i+=4){
-            imgData.data[i] = 100;
-        }*/
-    }
-
-    if(y + img.height + moveY > canvas.height || y + moveY <= 0){
+    /*if (y + img.height + moveY > canvas.height || y + moveY <= 0) {
         moveY *= -1;
-        /*for(var i = 0; i < imgData.data.length; i+=4){
-            imgData.data[i] = 200;
-        }*/
-    }
-    x += moveX;
-    y += moveY;
+    }*/
+    /*x += moveX;
+    y += moveY;*/
 }
 
-function rect_draw(){
+function rect_draw() {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.w, this.h); 
+    ctx.fillRect(this.x, this.y, this.w, this.h);
 }
 
-function handleStart(ev){
+function circle_print(obj) {
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.arc(obj.x, obj.y, obj.rayon, 0, Math.PI * 2, obj.sensAntiHoraire); // obj.offset + frame / 30, 2 * Math.PI + frame / 30
+    ctx.fill();
+    ctx.stroke();
+}
+
+function handleStart(ev) {
     ev.preventDefault();
 
     var touches = ev.changedTouches;
     var touchX = 0;
     var touchY = 0;
 
-    for (var i=0; i<touches.length; i++) {
+    for (var i = 0; i < touches.length; i++) {
         ongoingTouches.push(touches[i]);
         var color = colorForTouch(touches[i]);
         ctx.fillStyle = color;
-        ctx.fillRect(touches[i].pageX-2, touches[i].pageY-2, 4, 4);
-      }
+        ctx.fillRect(touches[i].pageX - 2, touches[i].pageY - 2, 4, 4);
+    }
 }
 
-function handleEnd(ev){
+function handleEnd(ev) {
     ev.preventDefault();
 }
 
-function handleCancel(ev){
+function handleCancel(ev) {
     ev.preventDefault();
 }
 
-function handleMove(ev){
+function handleMove(ev) {
     ev.preventDefault();
 }
 
+generateCircle();
 setInterval(gameLoop, 1000 / 60);
